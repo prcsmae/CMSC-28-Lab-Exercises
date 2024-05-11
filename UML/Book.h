36d597f5379ed1a7cf09b5ec0c8f0ea7885bdef1
prcsmae <106019380+prcsmae@users.noreply.github.com>
2024-05-11 21:14:37 +0800

    added documentation

--- UML/Book.h
+++ UML/Book.h
@@ -1,49 +1,65 @@
+// Jomuad - UML Class Diagram
+
 #include <iostream>
 #include <string>
+
+// Use string from standard namespace
 using std::string;
 
+// Define the Book class
 class Book{
+    // Private member variables
     string title;
     string author;
     string isbn;
     string publisher;
 
 public:
+    // Public constructor
+    // Initializes a new instance of Book class with the given arguments
     Book(const string& title, const string& author, const string& isbn, const string& publisher)
         : title(title), author(author), isbn(isbn), publisher(publisher) {
     }
 
+    // Returns the title
     string getTitle(){
         return title;
     }
 
+    // Returns author
     string getAuthor(){
         return author;
     }
 
+    // Returns ISBN
     string getISBN(){
         return isbn;
     }
 
+    // Returns publisher
     string getPublisher(){
         return publisher;
     }
 
+    // Sets a new title and returns it
     string setTitle(const string& bktitle){
         title = bktitle;
         return title;
     }
 
+    // Sets a new author and returns it
     string setAuthor(const string& bkauthor){
         author = bkauthor;
         return author;
     }
 
+    // Sets a new ISBN and returns it
     string setISBN(const string& bkisbn){
         isbn = bkisbn;
         return isbn;
     }
 
+    // Sets a new publisher and returns it 
    string setPublisher(const string& bkpublisher){
         publisher = bkpublisher;
         return publisher;
--- UML/testBook.cpp
+++ UML/testBook.cpp
@@ -1,23 +1,32 @@
+// Jomuad - UML Class Diagram
+
 #include <iostream>
 #include <string>
-#include "Book.h"
+#include "Book.h" // Include the book class header file
 
+// Use string, cout, and cin from standard namespace
 using std::string;
 using std::cout;
 using std::cin;
 
 int main(){
+    // Create a new book object with initial values
     Book reading("I Hope This Doesn't Find You", "Ann Liang", "1338827162", "Scholastic Inc.");
+    
+    // Print out current book details
     cout << "\nI'm currently reading " << reading.getTitle() << " by " << reading.getAuthor();
     cout << "\nPublished by " << reading.getPublisher() << ". It's ISBN is: " << reading.getISBN() << ".";
 
+    // Change the book details
     reading.setTitle("A Thousand Splendid Suns");
     reading.setAuthor("Khaled Hosseini");
     reading.setISBN("978-1-59448-950-1");
     reading.setPublisher("Riverhead Books");
 
+    // Print out new book details
     cout << "\n\nIf I decide to drop it I'll be reading " << reading.getTitle() << " next.\n";
     cout << "It is written by " << reading.getAuthor() << ". Published by " << reading.getPublisher();
     cout << " with ISBN: " << reading.getISBN() << ".";
+    
     return 0;
 }
