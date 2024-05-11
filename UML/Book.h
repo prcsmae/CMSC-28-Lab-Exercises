4a3ba0b2b985fc787a360ec76c2f34769677c4b3
prcsmae <106019380+prcsmae@users.noreply.github.com>
2024-05-11 21:34:06 +0800

    changed return type for set methods

--- UML/Book.h
+++ UML/Book.h
@@ -42,26 +42,23 @@ public:
     }
 
     // Sets a new title and returns it
-    string setTitle(const string& bktitle){
+    void setTitle(const string& bktitle){
         title = bktitle;
         return title;
     }
 
     // Sets a new author and returns it
-    string setAuthor(const string& bkauthor){
+    void setAuthor(const string& bkauthor){
         author = bkauthor;
-        return author;
     }
 
     // Sets a new ISBN and returns it
-    string setISBN(const string& bkisbn){
+    void setISBN(const string& bkisbn){
         isbn = bkisbn;
-        return isbn;
     }
 
     // Sets a new publisher and returns it 
-   string setPublisher(const string& bkpublisher){
+   void setPublisher(const string& bkpublisher){
         publisher = bkpublisher;
-        return publisher;
    }
 };
